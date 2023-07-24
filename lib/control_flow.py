f25aea70e402620076ba8702f8c5bd2884becd36
Alyssa <alyssa.brooksong@gmail.com>
2023-07-23 20:26:59 -0700

    completed assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,71 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
+    if username == "admin" and password == "12345" or username == "ADMIN" and password == "12345":
+        return 'Access granted'
+    else:
+        return 'Access denied'
     # your code here
     pass
 
 def hows_the_weather(temperature):
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature > 40 and temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
     # your code here
     pass
 
 def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 ==0:
+        return 'FizzBuzz'
+    elif num % 3 == 0:
+        return 'Fizz'
+    elif num % 5 == 0:
+        return 'Buzz'
+    else:
+        return num
     # your code here
     pass
 
 def calculator(operation, num1, num2):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print('Invalid operation!')
+        return None
     # your code here
     pass
+
+def divide(num1, num2):
+    try:
+        quotient = num1 / num2
+        print(quotient)
+    except ZeroDivisionError:
+        print("Error: num2 cannot be equal to 0")
+    except TypeError:
+        print("Error: input must be of type int or float")
+    finally:
+        print("Isn't division fun?")
+
+dog = "cuddly"
+
+dict_map = {
+    "hungry": "Refilling food bowl.",
+    "thirsty": "Refilling water bowl.",
+    "playful": "Playing tug-of-war.",
+    "cuddly": "Snuggling.",
+}
+
+# Remember that a dictionary's .get() method lets us set a default value!
+owner = dict_map.get(dog, "Reading newspaper.")
